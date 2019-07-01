9b5cfafdd7736796b372a001c12d331273c4c207
umesheo <umesh.budathoki23@gmail.com>
2019-07-01 17:01:39 +0545

    The main page

---
+++ index.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Crud App</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php require_once 'process.php'; ?>
+  
+  <?php 
+  if (isset($_SESSION['message'])): ?>
+  <div class="alert alert-<?=$_SESSION['msg_type']?>">
+  
+  <?php
+      echo $_SESSION['message'];
+      unset($_SESSION['message']); 
+  
+  ?>
+  </div>
+  <?php endif ?>
+ <!-- Displaying data from php database crud] -->
+  <div class="container">
+ 
+  <?php
+     $servername="localhost";
+     $username="root";
+     $password="";
+     $dbname="crud";
+     $con= new mysqli($servername,$username,$password,$dbname); 
+     if ($con->connect_error)
+     {
+         die("connection failed: " . $conn->connect_error);
+     
+     }
+     $Movie_sql1="SELECT * FROM data";
+    $result=$con->query($Movie_sql1);
+     //pre_r($result);
+    ?>
+    
+    <div class="row justify-content-center">
+      <table class="table">
+        <thead>
+          <tr>
+            <th>Movie Name</th>
+            <th colspan="2">Action</th>
+            </thead>
+            <?php
+              while ($row = $result->fetch_assoc()): ?>
+                <tr>
+                  <td><?php echo $row['MovieName']; ?></td>
+                  <td>
+                    <a href="index.php?edit=<?php echo $row['ID']; ?>"
+                    class="btn btn-info">Edit</a>
+                    <a href="index.php?delete=<?php echo $row['ID']; ?>"
+                    class="btn btn-danger">Delete</a>
+                  </td>
+                  </tr>
+              <?php endwhile; ?>
+            </table>
+            </div>
+    <?php
+     function pre_r( $array ){
+       echo '<pre>';
+       print_r($array);
+       echo '</pre>';
+     }
+  
+  ?>
+ 
+  <!-- Displaying the form -->
+  <div class="row justify-content-center">
+    <form actiom="process.php" method="POST">
+    <input type="hidden" name="ID" value="<?php echo $ID; ?>">
+    <div class="form-group">
+        <label>Movie Name</label>
+        <input type="text" name="MovieName" class="form-control" value="<?php echo $MovieName; ?>" placeholder="Enter your movie name">
+       </div>
+       <div class="form-group">
+       <?php
+       if ($update == true):
+        ?>
+          <button type="submit" class="btn btn-primary" name="update">Update</button>
+       <?php else: ?>
+          <button type="submit" class="btn btn-primary" name="save">Save</button>
+       <?php endif; ?>
+        </div>
+        </form>
+        </div>
+        </div>
+
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
